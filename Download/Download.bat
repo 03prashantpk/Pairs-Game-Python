89eec2846425af721e4c7896532f5024cbc7024e
prashant kumar <prashantmanwan@gmail.com>
2021-11-13 14:11:03 +0530

    Version check

--- Download/Download.bat
+++ Download/Download.bat
@@ -2,9 +2,8 @@
 timeout 5
 
 Echo =======================================================
-Echo       Developed by - Prashant Kumar
+Echo       Developed by - Prashant Kumar and Team Members
 Echo =======================================================
-Pause
 
 @echo off
 
@@ -18,18 +17,18 @@ git clone https://github.com/03prashantpk/Pairs-Game-Python.git
 
 Echo =============================================
 Echo              Download Success!
-Echo     Location C:\\Games\Pairs-Game-Python 
+Echo     Location C:\Games\Pairs-Game-Python 
 Echo =============================================
-
-
-Echo =============================================
-Echo   Press ENTER to Start the Game
-Echo =============================================
-
-pause
 
 @echo off
 cd Pairs-Game-Python/
 start success.bat
+
+Echo =============================================
+Echo   Launching The Game... 
+Echo ============================================
+
+timeout 5
+
 start startGame.bat
 exit
