6ea452b5a318d10615260e34ef3aca85917159ff
prashant kumar <prashantmanwan@gmail.com>
2021-11-15 01:07:27 +0530

    helper improved

--- helper.py
+++ helper.py
@@ -13,11 +13,3 @@ parent = "C:\Games"
 shutil.rmtree("C:\Games\Pairs-Game-Python", ignore_errors=True)
 
 time.sleep(1)
-
-# Path 
-#path = os.path.join(parent, directory) 
-
-
-    
-# Remove the Directory to download new updates
-#os.rmdir(path)
